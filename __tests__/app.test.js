1273ce770b00ad29ef33dd4e7f789bae90894543
internetcharles <charlesodellsmith@gmail.com>
2020-08-18 15:12:20 -0700

    post works

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -6,39 +6,40 @@ const fakeRequest = require('supertest');
 const app = require('../lib/app');
 const client = require('../lib/client');
 
-describe('app routes', () => {
-  beforeAll(done => {
-    return client.connect(done);
+  describe('routes', () => {
+    let token;
+
+  beforeAll(async done => {
+    execSync('npm run setup-db');
+    client.connect();
+    const signInData = await fakeRequest(app)
+      .post('/auth/signup')
+      .send({
+        email: 'jon@user.com',
+        password: '1234'
+      });
+    token = signInData.body.token;
+    return done();
   });
 
-  beforeEach(() => {
-    // TODO: ADD DROP SETUP DB SCRIPT
-    execSync('npm run setup-db');
-  });
 
   afterAll(done => {
     return client.end(done);
   });
 
-  test('returns animals', async() => {
+  test('returns todos', async() => {
 
     const expectation = [
       {
         'id': 1,
-        'name': 'bessie',
-        'coolfactor': 3,
+        'todo': 'take out trash',
+        'completed': false,
         'owner_id': 1
       },
       {
         'id': 2,
-        'name': 'jumpy',
-        'coolfactor': 4,
-        'owner_id': 1
-      },
-      {
-        'id': 3,
-        'name': 'spot',
-        'coolfactor': 10,
+        'todo': 'complete lab',
+        'completed': false,
         'owner_id': 1
       }
     ];
